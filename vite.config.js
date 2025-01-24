0cd29f31b7e521e5c245de3d063f2aa6242ed93e
Mahmudjon4419 <anasxon245@gmail.com>
2025-01-25 01:06:06 +0500

    boldi shekilli

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// filepath: /c:/Users/Lenovo/Desktop/react_first/my-first-react/vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/first-portfolio/', // Bu yerda 'first-portfolio' ni 'homepage' URL manziliga moslang
+});
